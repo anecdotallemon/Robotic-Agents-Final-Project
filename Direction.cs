66171279120a929675548c5336a6cb58ef53c20c
skylord-a52 <skylorda52@gmail.com>
2022-01-12 15:52:25 -0800

    added documentation

--- Direction.cs
+++ Direction.cs
@@ -1,22 +1,51 @@
 ﻿namespace DefaultNamespace {
+    
+    /// <summary>
+    /// "Pseudo-enum" representing cardinal directions in 2D space
+    /// </summary>
     class Direction {
         
         #region Static
-
+        
+        /// <summary>
+        /// Constant field representing the direction up.
+        /// </summary>
         public static readonly Direction Up = new Direction(new Point(0, -1));
+        /// <summary>
+        /// Constant field representing the direction right.
+        /// </summary>
         public static readonly Direction Right = new Direction(new Point(1, 0));
+        /// <summary>
+        /// Constant field representing the direction down.
+        /// </summary>
         public static readonly Direction Down = new Direction(new Point(0, 1));
+        /// <summary>
+        /// Constant field representing the direction left.
+        /// </summary>
         public static readonly Direction Left = new Direction(new Point(-1, 0));
-
+        
+        /// <summary>
+        /// A list of all cardinal <c>Direction</c>s, for iterating over
+        /// </summary>
         public static readonly Direction[] Directions = {Up, Right, Down, Left};
-
+        
+        /// <summary>
+        /// A dictionary organized by the <c>Point</c> offset of each direction, for converting offsets to <c>Direction</c>s.
+        /// </summary>
         public static readonly Dictionary<Point, Direction> FromOffset = new Dictionary<Point, Direction>() {
             {Up.Offset, Up},
             {Right.Offset, Right},
             {Down.Offset, Down},
             {Left.Offset, Left}
         };
-
+        
+        /// <summary>
+        /// Given two points, return the <c>Direction<c> that moves between them.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns>A <c>Direction</c> representing the vector between the given <c>Point</c>s.</returns>
+        /// <exception cref="ArgumentException">If the two points are not exactly one unit apart</exception>
         public static Direction FromPoints(Point start, Point end) {
             try {
                 return FromOffset[end - start];
@@ -30,13 +59,21 @@
         #endregion
 
         #region NonStatic
-
+        
+        /// <summary>
+        /// A <c>Point</c> representing one space of movement in the given direction.
+        /// </summary>
         public readonly Point Offset;
 
         private Direction(Point p) {
             Offset = p;
         }
-
+        
+        /// <summary>
+        /// Apply the <c>Direction</c> to the given <c>Point</c>, returning a copy moved in this direction one square.
+        /// </summary>
+        /// <param name="p">The <c>Point</c> to move</param>
+        /// <returns>A copy of the given <c>Point</c>, moved one square in this <c>Direction</c></returns>
         public Point ApplyToPoint(Point p) {
             return Offset + p;
         }
