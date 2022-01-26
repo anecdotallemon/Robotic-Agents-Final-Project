dcd8593b351af5d7b9faf365ad88124dc593c0ac
skylord-a52 <skylorda52@gmail.com>
2022-01-26 13:56:59 -0800

    fixed some access issues

--- State.cs
+++ State.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using Robotic_Agents_Final_Project;
 
 namespace Robotic_Agents_Final_Project
 {
     public class State
     {
-        // Constructor with gameBoard initialization
-        public State(int width, int height)
-        {
-            public Point[,] gameBoard = new[width, height];
-        }
 
         // one list for our pacs and one list for enemies, since we may want to keep track of where we ares
         public List < Player > MyPacs { get; private set; }
         public List<Player> Enemies { get; private set; }
-
+        public Point[,] GameBoard;
+        
+        // Constructor with gameBoard initialization
+        public State(int width, int height) {
+            GameBoard = new Point[width, height];
+        }
+        
         public State Clone() {
             throw new NotImplementedException();
         }
