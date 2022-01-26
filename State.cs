7f61d0c4d138071e28f494caf627da6cd54022af
skylord-a52 <skylorda52@gmail.com>
2022-01-26 15:23:53 -0800

    state clone

--- State.cs
+++ State.cs
@@ -11,7 +11,16 @@ namespace Robotic_Agents_Final_Project
         public List < Player > MyPacs { get; private set; }
         public List<Player> Enemies { get; private set; }
         public Point[,] GameBoard;
+
+        private Queue<Player> _turnOrder = new Queue<Player>(); // all alive players, sorted by turn order
+        private List<Player> _allPlayers = new List<Player>(); // list of all players, including dead ones, sorted by initial turn order
+
+        private bool[,] _walls;
+        private int[,] _scores;
         
+        public readonly int Width;
+        public readonly int Height;
+
         // Constructor with gameBoard initialization
         public State(Point[,] GameBoard, List < Player > MyPacs,  List<Player> Enemies   ) {
             this.GameBoard = new Point[GmaeBoard.GetLength(0) , GameBoard.GetLength(1)] ;
@@ -32,7 +41,42 @@ namespace Robotic_Agents_Final_Project
         }
         
         public State Clone() {
-            throw new NotImplementedException();
+
+            State stateCopy = new State(Width, Height);
+        
+            // clone all players
+            foreach (Player p in _allPlayers) {
+
+                Player playerCopy = p.Clone();
+            
+                // add to turn orders
+                stateCopy._allPlayers.Add(playerCopy);
+                if (p.Alive) {
+                    stateCopy._turnOrder.Enqueue(playerCopy);
+                }
+            
+                // add to lists
+                if (p.IsOurPlayer) {
+                    stateCopy.MyPacs.Add(playerCopy);
+                }
+                else {
+                    stateCopy.Enemies.Add(playerCopy);
+                }
+            }
+        
+            // shuffle the copy's turn order queue to match ours
+            while (!stateCopy._turnOrder.Peek().Equals(_turnOrder.Peek())) {
+                stateCopy._turnOrder.Enqueue(stateCopy._turnOrder.Dequeue());
+            }
+        
+        
+            for (int i = 0; i < Width; i++) {
+                for (int j = 0; j < Height; j++) {
+                    stateCopy._walls[i, j] = _walls[i,j];
+                }
+            }
+
+            return stateCopy;
         }
 
         public Player GetCurrentPlayer() {
