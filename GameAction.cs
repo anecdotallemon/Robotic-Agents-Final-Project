40da6a7103bb988ea168f621219da89142b12aed
skylord-a52 <skylorda52@gmail.com>
2022-01-25 19:34:00 -0800

    game action class

---
+++ GameAction.cs
@@ -0,0 +1,6 @@
+﻿namespace Robotic_Agents_Final_Project {
+    // class to represent actions -- cant just use Direction because this game lets you speed, change color, etc
+    public class GameAction {
+        
+    }
+}
