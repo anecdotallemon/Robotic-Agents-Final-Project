f51a6c8a698f8131c5dd5bd36f22ac970ce5acee
skylord-a52 <skylorda52@gmail.com>
2022-01-26 16:14:02 -0800

    pellet class (purely for input purposes, not game logic)

---
+++ Pellet.cs
@@ -0,0 +1,13 @@
+﻿namespace Robotic_Agents_Final_Project {
+    // simple pellet class
+    // not actually used in game logic, just for transferring information from parser -> state
+    public class Pellet {
+        public Point Location;
+        public int Score;
+
+        public Pellet(Point location, int score) {
+            Location = location;
+            Score = score;
+        }
+    }
+}
